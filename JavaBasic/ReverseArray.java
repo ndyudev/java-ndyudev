2999f1a330772575c313f579b4b19972f1aab915
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-01-03 22:32:48 +0700

    Debug ReverseArray

--- JavaBasic/ReverseArray.java
+++ JavaBasic/ReverseArray.java
@@ -7,15 +7,15 @@ public class ReverseArray {
         System.out.print("Nhập số phần tử của mảng: ");
         int n = scanner.nextInt();
         int[] arr = new int[n];
-
         System.out.println("Nhập các phần tử:");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-
+        
         System.out.println("Mảng sau khi đảo ngược:");
         for (int i = n - 1; i >= 0; i--) {
             System.out.print(arr[i] + " ");
         }
+        scanner.close();
     }
 }
