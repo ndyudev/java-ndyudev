81e8145441fa9ca3bafd8659cb6b11533f961ad3
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-01-24 20:53:01 +0700

    Debug Dem Ky Tu

--- JavaBasic/DemKyTu.java
+++ JavaBasic/DemKyTu.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class DemKyTu {
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Nhập vào một chuỗi: ");
         String str = sc.nextLine();
 
         int kyTu = 0, so = 0, khoangTrang = 0;
-
         for (char c : str.toCharArray()) {
             if (Character.isLetter(c)) {
                 kyTu++;
@@ -18,7 +18,6 @@ public class DemKyTu {
                 khoangTrang++;
             }
         }
-
         System.out.println("Số ký tự: " + kyTu);
         System.out.println("Số chữ số: " + so);
         System.out.println("Số khoảng trắng: " + khoangTrang);
